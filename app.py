0a55d91d8f144ffdf8e41e1afc1581ea68fd9ce8
talhakhan195 <talhabhai19@gmail.com>
2024-12-23 03:13:40 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+
+app= Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def servies():
+    return render_template('services.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
